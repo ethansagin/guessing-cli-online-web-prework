dcfe8c5feddde6da465b5b31154c1345526f84cd
Ethan <ethansagin@gmail.com>
2019-03-24 21:20:56 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,9 +3,9 @@
 #- Printing out a statement You guessed the correct number! if the number has been guessed correctly, or The computer guessed <number>. if the number has been guessed incorrectly 
 #- Allowing the user to exit the program when exit is the input
 
-def random_number
-  rand
-  rand
+def guessing_game
+  
+end
 
 
 
