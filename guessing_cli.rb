817467c5d25c48cb0b1e5d6580eecf78e7b8580e
Ethan <ethansagin@gmail.com>
2019-03-24 21:49:20 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,9 +3,9 @@
 #- Printing out a statement You guessed the correct number! if the number has been guessed correctly, or The computer guessed <number>. if the number has been guessed incorrectly 
 #- Allowing the user to exit the program when exit is the input
 
-def comp_op
+def guess_input
   pc = rand(1..6)
-  
+  person = gets.chomp
 end
 
 def exit_game
@@ -14,8 +14,7 @@ end
 
 def run_guessing_game
   puts "Guess a number between 1 and 6."
-  input = gets.chomp
-  rand_num
+  guess_input
   until input == "exit"
     case input
     when input.between?(1, 6)
