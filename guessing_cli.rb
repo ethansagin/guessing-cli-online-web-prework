fa27185cda6886593a64b3ca6332343908eb65be
Ethan <ethansagin@gmail.com>
2019-03-24 21:19:00 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,7 +2,9 @@
 #- Comparing that input to a random number that has been generated 
 #- Printing out a statement You guessed the correct number! if the number has been guessed correctly, or The computer guessed <number>. if the number has been guessed incorrectly 
 #- Allowing the user to exit the program when exit is the input
-def 
+
+def random_number
+  rand
 
 
 
