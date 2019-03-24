a5982e26becc5e5177421bb8ff6ed42dcdbcad6b
Ethan <ethansagin@gmail.com>
2019-03-24 21:18:13 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,3 +1,6 @@
+#Taking in input from the command line 
+#- Comparing that input to a random number that has been generated 
+#- Printing out a statement You guessed the correct number! if the number has been guessed correctly, or The computer guessed <number>. if the number has been guessed incorrectly - Allowing the user to exit the program when exit is the input
 def 
 
 
