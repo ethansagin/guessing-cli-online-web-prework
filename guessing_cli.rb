fc6bd7d9519667fe7de5e30341453a9fa05f9b01
Ethan <ethansagin@gmail.com>
2019-03-24 21:54:40 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,13 +3,13 @@
 #- Printing out a statement You guessed the correct number! if the number has been guessed correctly, or The computer guessed <number>. if the number has been guessed incorrectly 
 #- Allowing the user to exit the program when exit is the input
 
-
+require 'pry'
 
 def run_guessing_game
   puts "Guess a number between 1 and 6."
   pc = rand(1..6)
   person = gets.chomp
-  
+  binding.pry
   until person == "exit"
     case person
     when person.between?(1, 6)
